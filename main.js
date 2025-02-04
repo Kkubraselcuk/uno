ca7915b9a38d30858d29e8443eed4c4f3cea344e
Kübra SELÇUK <77327246+Kkubraselcuk@users.noreply.github.com>
2025-02-04 15:46:17 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// Returns a random integer from 0 to 9:
+Math.floor(Math.random() * 10);
+
+var kolay = ["1 kart çek","2 kart çek","3 kart çek"];
+
+var orta = ["4 kart çek","5 kart çek","6 kart çek",
+    "elindeki mavi kartları yere bırak",
+    "elindeki yeşil kartları yere bırak",
+    "elindeki kırmızı kartları yere bırak",
+    "elindeki sarı kartları yere bırak"];
+
+var zor = ["7 kart çek", 
+    "8 kart çek", 
+    "9 kart çek",
+    "elindeki tüm özel kartları yere bırak"];
+
+const kolayBtn = () =>{
+    var randomKolayKart = kolay[Math.floor(Math.random() * kolay.length)];
+    alert(randomKolayKart);
+}
+const ortaBtn = () =>{
+    var randomOrtaKart = orta[Math.floor(Math.random() * orta.length)];
+    alert(randomOrtaKart);
+}
+const zorBtn = () =>{
+    var randomZorKart = zor[Math.floor(Math.random() * zor.length)];
+    alert(randomZorKart);
+}
