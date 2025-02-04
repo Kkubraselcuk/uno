0a76ca89b1a92c22d324898a4f4700de74982074
Kübra SELÇUK <77327246+Kkubraselcuk@users.noreply.github.com>
2025-02-04 16:34:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,17 @@ var zor = ["7 kart çek",
     "9 kart çek",
     "elindeki tüm özel kartları yere bırak"];
 
+var rastgele = ["1 kart çek","2 kart çek","3 kart çek",
+    "4 kart çek","5 kart çek","6 kart çek",
+    "elindeki mavi kartları yere bırak",
+    "elindeki yeşil kartları yere bırak",
+    "elindeki kırmızı kartları yere bırak",
+    "elindeki sarı kartları yere bırak",
+    "7 kart çek", 
+    "8 kart çek", 
+    "9 kart çek",
+    "elindeki tüm özel kartları yere bırak"];
+
 const kolayBtn = () =>{
     var randomKolayKart = kolay[Math.floor(Math.random() * kolay.length)];
     alert(randomKolayKart);
@@ -25,4 +36,9 @@ const ortaBtn = () =>{
 const zorBtn = () =>{
     var randomZorKart = zor[Math.floor(Math.random() * zor.length)];
     alert(randomZorKart);
+}
+
+const rastgeleBtn = () =>{
+    var randomRastgeleKart = rastgele[Math.floor(Math.random() * rastgele.length)];
+    alert(randomRastgeleKart);
 }
